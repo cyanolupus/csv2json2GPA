625827d72ea6ac4c77ca3ebd6d804d2afb54f1f6
cyanolupus <cyanolupus@gmail.com>
2021-03-21 20:49:42 +0900

    first commit

---
+++ csv2json.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cat $1 | \
+sed -e "s/\"//g" -e "s/\r//g" -e "s/\ //g" | \
+jq -s -R 'split("\n")|map(split(","))|map({"student_id": .[0], "student_name": .[1], "unit_id": .[2], "unit_name": .[3], "unit_num": .[4], "spring_grade": .[5], "autumn_grade": .[6], "grade": .[7], "unit_group": .[8], "year": .[9], "group": .[10]})'
---
+++ json2gpa.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+./csv2json.sh $1 | \
+jq -r 'del(.[0,-1])' | \
+jq -r 'map(select(.unit_group != "C0" and .grade != "認" and .grade != "P" and .grade != "F")|{"grade": .grade, "unit_num": (.unit_num|tonumber*10)})' | \
+sed -e "s/A+/43/g" -e "s/A/40/g" -e "s/B/30/g" -e "s/C/20/g" -e "s/D/0/g" | \
+jq '{"units_sum":(map(.unit_num * 10)|add), "gp_sum":(map(.unit_num * (.grade|tonumber))|add)}' | \
+jq -r '{"Units_sum":(.units_sum/100), "GP_sum":(.gp_sum/100), "GPA":(.gp_sum/.units_sum)}'
